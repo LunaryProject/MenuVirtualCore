b0133fd09c3d65be557a93672279d95921681979
João Pedro <gustavo.s@aluno.senai.br>
2022-11-09 18:24:06 -0300

    .

--- MenuLunary/MenuLunary/Controllers/QRCoderController.cs
+++ MenuLunary/MenuLunary/Controllers/QRCoderController.cs
@@ -1,9 +1,6 @@
-﻿using MenuLunary.Models;
-using QRCoder;
-using System;
+﻿using QRCoder;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using System.Web.Mvc;
 
 namespace MenuLunary.Controllers
@@ -16,20 +13,20 @@ namespace MenuLunary.Controllers
             return View();
         }
 
+
+
         [HttpPost]
         public ActionResult QR(string qrcode)
         {
             using (MemoryStream ms = new MemoryStream())
             {
-                
-
                 qrcode = Request.Url.Authority + "/Restaurante/Menu";
 
                 QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
                 QRCodeData qrCodeData = qRCodeGenerator.CreateQrCode(qrcode, QRCodeGenerator.ECCLevel.Q);
                 QRCode qrCode = new QRCode(qrCodeData);
 
-                using (Bitmap bitmap = qrCode.GetGraphic(20))
+                using (Bitmap bitmap = qrCode.GetGraphic(10))
                 {
                     bitmap.Save(ms, ImageFormat.Png);
                     ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
