9688106f513e15f9445904cba444bd3e128d54da
LuizGuilherme0 <luiz.barreto@aluno.senai.br>
2022-11-09 18:21:01 -0300

    att

--- MenuLunary/MenuLunary/Controllers/QRCoderController.cs
+++ MenuLunary/MenuLunary/Controllers/QRCoderController.cs
@@ -1,37 +1,40 @@
 ﻿using MenuLunary.Models;
+using Microsoft.AspNetCore.Mvc;
 using QRCoder;
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Web.Mvc;
+using Controller = Microsoft.AspNetCore.Mvc.Controller;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
 
 namespace MenuLunary.Controllers
 {
     public class QRCoderController : Controller
     {
         // GET: QrCode
-        public ActionResult QR()
+        public IActionResult QR()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult QR(string qrcode)
+        public IActionResult QR(string qrcode)
         {
             using (MemoryStream ms = new MemoryStream())
             {
                 
 
-                qrcode = Request.Url.Authority + "/Restaurante/Menu";
+                qrcode = "/Restaurante/Menu" + Request.Url.Authority;
 
-                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
-                QRCodeData qrCodeData = qRCodeGenerator.CreateQrCode(qrcode, QRCodeGenerator.ECCLevel.Q);
-                QRCode qrCode = new QRCode(qrCodeData);
+                QRCodeGenerator QRCodeGenerator = new QRCodeGenerator();
+                QRCodeData QRCodeData = QRCodeGenerator.CreateQrCode(qrcode, QRCodeGenerator.ECCLevel.Q);
+                QRCode QRCode = new QRCode(QRCodeData);
 
-                using (var bitmap = qrCode.GetGraphic(20))
+                using (Bitmap Bitmap = QRCode.GetGraphic(20))
                 {
-                    bitmap.Save(ms, ImageFormat.Png);
+                    Bitmap.Save(ms, ImageFormat.Png);
                     ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
                 }
             }
@@ -40,15 +43,5 @@ namespace MenuLunary.Controllers
 
     }
 
-    internal class QRCode
-    {
-        public QRCode(QRCodeData qrCodeData)
-        {
-        }
-
-        internal Bitmap GetGraphic(int v)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    
 }
