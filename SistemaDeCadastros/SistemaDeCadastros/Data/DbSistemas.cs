d8a3ce10f5df90de11b384b95e7e2f9d62f35fe3
João Pedro <gustavo.s@aluno.senai.br>
2022-11-30 17:17:42 -0300

    .

---
+++ SistemaDeCadastros/SistemaDeCadastros/Data/DbSistemas.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeCadastros.Models;
+
+namespace SistemaDeCadastros.Data
+{
+    public class DbSistemas : DbContext
+    {
+        public DbSistemas(DbContextOptions<DbSistemas> options) : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
