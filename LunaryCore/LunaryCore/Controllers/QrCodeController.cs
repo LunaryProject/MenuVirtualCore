89a1d35317a2684a07337e138f7ee0a5fdcefe13
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2022-12-12 11:59:29 -0300

    .

--- LunaryCore/LunaryCore/Controllers/QrCodeController.cs
+++ LunaryCore/LunaryCore/Controllers/QrCodeController.cs
@@ -30,8 +30,8 @@ namespace MenuLunary.Controllers
             {
                 //qrcode = Path.GetPathRoot(qrcode) + "/Restaurante/Menu";
                 //ViewBag.Url = Path.GetPathRoot(qrcode) + "/Restaurante/Menu";
-                qrcode = Path.GetPathRoot(qrcode) + "http://localhost:5201/Restaurante/Menu";
-                
+                qrcode = Path.GetPathRoot(qrcode) + "https://restaurantelunary.herokuapp.com/Restaurante/Menu";
+
                 QRCodeGenerator QRCodeGenerator = new QRCodeGenerator();
                 QRCodeData QRCodeData = QRCodeGenerator.CreateQrCode(qrcode, QRCodeGenerator.ECCLevel.Q);
                 QRCode QRCode = new QRCode(QRCodeData);
