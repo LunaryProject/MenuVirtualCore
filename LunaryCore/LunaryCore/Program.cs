c9a41dacb6622d2bb2b8a8ba1055241557c46ce9
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2022-12-12 20:55:51 -0300

    .

--- LunaryCore/LunaryCore/Program.cs
+++ LunaryCore/LunaryCore/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Contexto>(options => options.UseSqlServer
 ("Data Source=mssql2016.hostingzone.com.br;Initial Catalog=lunary;user id=lunary;password=lunary;Integrated Security=false;Encrypt=False"));
+//("Data Source=sql.bsite.net\\MSSQL2016;Initial Catalog=lunarymenuvirtual_;user id=lunarymenuvirtual_;password=lunary123;Integrated Security=false;Encrypt=False"));
 //("Data Source=.\\SENAI;Initial Catalog=BancoDeDados;Integrated Security=True;Encrypt=False"));
 //("Data Source=10.139.166.53\\SENAI;Initial Catalog=BancoDeDados;Integrated Security=True;Encrypt=False"));
 
